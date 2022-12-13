94d5b9eb2143efaff3bb4f9aaf7dd1cc3ce0f961
killercho <ggeorgi60@gmail.com>
2022-12-13 16:55:30 +0200

    Added default text.
    This includes scores and player names.

--- game.py
+++ game.py
@@ -12,9 +12,18 @@ WHITE = (255, 255, 255)
 
 LINES_WIDTH = 8
 
+INGAME_TEXT_FONT = pygame.font.Font(None, 100)
+
+TOP_LINE_Y = 100
+
 size = (1280, 720)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Super Pong")
+
+a_name = "Player 1"
+b_name = "Player 2"
+a_score = 0
+b_score = 0
 
 clock = pygame.time.Clock()
 
@@ -31,10 +40,23 @@ while game_running:
     screen.fill(BLACK)
 
     # creating punctured line
-    for i in range(10):
-        vertical_line = pygame.Surface((1, 64), pygame.SRCALPHA)
+    for i in range(0, 720, 72):
+        vertical_line = pygame.Surface((8, 64), pygame.SRCALPHA)
         vertical_line.fill((255, 255, 255, 70))
-        screen.blit(vertical_line, (636, i * 72))
+        screen.blit(vertical_line, (636, i))
+
+    pygame.draw.line(screen, WHITE, [0, TOP_LINE_Y], [1280, TOP_LINE_Y], 8)
+    pygame.draw.line(screen, WHITE, [639, 0], [639, 100], 8)
+
+    text = INGAME_TEXT_FONT.render(str(a_score), 1, WHITE)
+    screen.blit(text, (532, 20))
+    text = INGAME_TEXT_FONT.render(str(b_score), 1, WHITE)
+    screen.blit(text, (722, 20))
+
+    text = INGAME_TEXT_FONT.render(a_name, 1, WHITE)
+    screen.blit(text, (100, 20))
+    text = INGAME_TEXT_FONT.render(b_name, 1, WHITE)
+    screen.blit(text, (854, 20))
 
     pygame.display.flip()
     clock.tick(60)
