6d43f9b4cecaa4907c5f6ed9994df8731e567f0c
killercho <ggeorgi60@gmail.com>
2023-01-27 20:17:33 +0200

    Added network related menus.
    This includes the menu for the host and the one for the one joining.

--- main_menu.py
+++ main_menu.py
@@ -13,35 +13,41 @@ class Menu:
         self.__screen = pygame.display.set_mode(c.SCREEN_SIZE)
         pygame.display.set_caption("Super Pong")
 
+        # Menu declarations:
         self.__main_menu = pygame_menu.Menu("Super Pong", c.SCREEN_SIZE[0], c.SCREEN_SIZE[1],
                                             theme=c.MENU_THEME)
-        # Start menu
         self.__start_menu = pygame_menu.Menu("Game settings", c.SCREEN_SIZE[0],
                                              c.SCREEN_SIZE[1], theme=c.MENU_THEME)
-
-        self.__name_1_input_field = self.__start_menu.add.text_input(
-            "Player 1 name: ", default="")
-        self.__name_2_input_field = self.__start_menu.add.text_input(
-            "Player 2 name: ", default="")
-
-        self.__start_menu.add.button("Start game", self.__start_game)
-
-        # Options menu
         self.__options_menu = pygame_menu.Menu("Options", c.SCREEN_SIZE[0], c.SCREEN_SIZE[1],
                                                theme=c.MENU_THEME)
-        # Options idea -> Music volume and sounds volume,
-        #       selector for what power-ups are in the game
-        self.__main_menu.add.button("Play", self.__start_menu)
+        self.__game_id_menu = pygame_menu.Menu(
+            "Game Id", c.SCREEN_SIZE[0] // 2, c.SCREEN_SIZE[1] // 2, theme=c.MENU_THEME)
+
+        # Main menu:
+        self.__name_input = self.__main_menu.add.text_input(
+            "Name: ", default="Guest")
+        self.__main_menu.add.button("Host", self.__start_menu)
+        self.__main_menu.add.button("Join", self.__game_id_menu)
         self.__main_menu.add.button("Options", self.__options_menu)
         self.__main_menu.add.button("Quit", pygame_menu.events.EXIT)
 
+        # Game Id menu:
+        self.__game_id = self.__game_id_menu.add.text_input("Id: ", default="")
+        self.__game_id_menu.add.button("Join game", self.__join_game)
+
+        # Host menu:
+        self.__player_label = self.__start_menu.add.label(
+            "Waiting for player...")
+        # Changing the message displayed -> self.__player_label.set_title("playert")
+        self.__start_menu.add.button("Start game", self.__start_game)
+
+        # Options menu
+        # Options idea -> Music volume and sounds volume,
+
         self.__main_menu.mainloop(self.__screen)
 
+    def __join_game(self):
+        pass
+
     def __start_game(self):
-        name_1_data: str = self.__name_1_input_field.get_value()[:c.MAX_NAME_SYMBOLS]
-        name_2_data: str = self.__name_2_input_field.get_value()[:c.MAX_NAME_SYMBOLS]
-
-        name_1: str = c.DEFAULT_NAME_1 if name_1_data == "" else name_1_data
-        name_2: str = c.DEFAULT_NAME_2 if name_2_data == "" else name_2_data
-
-        return Game(self.__screen, name_1, name_2)
+        return Game(self.__screen, self.__name_input.get_value(), c.DEFAULT_NAME_2)
